290df3c741208709be9ef554bf8f3df7892ff154
yinyangcolby <watersc@my.lanecc.edu>
2022-10-12 08:43:20 -0700

    main
    Main created

---
+++ ColbyWatersSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ColbyWatersSite.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
