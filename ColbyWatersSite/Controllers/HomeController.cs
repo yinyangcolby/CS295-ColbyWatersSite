b3640380f4a5ef96bda488d16c05316a7c649d01
yinyangcolby <watersc@my.lanecc.edu>
2022-10-12 22:24:19 -0700

    lab1
    lab1

--- ColbyWatersSite/Controllers/HomeController.cs
+++ ColbyWatersSite/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using ColbyWatersSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ColbyWatersSite.Controllers
 {
